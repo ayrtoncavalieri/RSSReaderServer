04339d3142867b1d3df52705989418b09a46aef4
Ayrton Cavalieri <ayrtoncavalieri@gmail.com>
2021-02-15 15:29:35 -0300

    Added optimization

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-g++ -g -DDEBUG -DSECSRV '-I src/' '-I src/opsImplementation' src/opsImplementation/*.cpp src/*.cpp -o RSSReaderServer.out -lPocoFoundation -lPocoNet -lPocoUtil -lPocoNetSSL -lPocoCrypto -lPocoData -lPocoDataMySQL -lPocoJSON -lPocoJWT
+g++ -g -O2 -DDEBUG -DSECSRV '-I src/' '-I src/opsImplementation' src/opsImplementation/*.cpp src/*.cpp -o RSSReaderServer.out -lPocoFoundation -lPocoNet -lPocoUtil -lPocoNetSSL -lPocoCrypto -lPocoData -lPocoDataMySQL -lPocoJSON -lPocoJWT
