a7060c3dd0f577e6213cfc53092f3d80a13939ec
Ayrton Cavalieri <ayrtoncavalieri@gmail.com>
2021-02-15 15:25:36 -0300

    Fixed typo

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-g++ -g -DDEBUG -DSECSVR '-I src/' '-I src/opsImplementation' src/opsImplementation/*.cpp src/*.cpp -o RSSReaderServer.out -lPocoFoundation -lPocoNet -lPocoUtil -lPocoNetSSL -lPocoCrypto -lPocoData -lPocoDataMySQL -lPocoJSON -lPocoJWT
+g++ -g -DDEBUG -DSECSRV '-I src/' '-I src/opsImplementation' src/opsImplementation/*.cpp src/*.cpp -o RSSReaderServer.out -lPocoFoundation -lPocoNet -lPocoUtil -lPocoNetSSL -lPocoCrypto -lPocoData -lPocoDataMySQL -lPocoJSON -lPocoJWT
