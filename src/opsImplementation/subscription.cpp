d7d7991e45fe550707c4fdcb52569da2e76bf954
Ayrton Cavalieri <ayrtoncavalieri@gmail.com>
2021-02-16 21:04:30 -0300

    Added license

--- src/opsImplementation/subscription.cpp
+++ src/opsImplementation/subscription.cpp
@@ -1,3 +1,20 @@
+/*
+    This file is part of RSSReaderServer.
+
+    RSSReaderServer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    RSSReaderServer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with RSSReaderServer.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "subscription.hpp"
 
 Poco::JSON::Object::Ptr subscription::subs(unsigned int op, Poco::JSON::Object::Ptr req, Poco::Data::Session &session, std::string salt)
