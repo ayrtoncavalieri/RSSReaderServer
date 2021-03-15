40ac70b2134135f41dff3c6e3f96d575bb99db90
Ayrton Cavalieri <ayrtoncavalieri@gmail.com>
2021-03-14 22:36:24 -0300

    Debugging

--- src/opsImplementation/logout.cpp
+++ src/opsImplementation/logout.cpp
@@ -4,12 +4,14 @@ Poco::JSON::Object::Ptr logout::_logout(unsigned int op, Poco::JSON::Object::Ptr
 {
     Poco::JSON::Object::Ptr reqResp;
     std::string email;
-    
+
     try{
         email = req->getValue<std::string>("email");
         std::string uuid = req->getValue<std::string>("uuid");
         Poco::toLowerInPlace(uuid);
-        session << "DELETE FROM `rssreader`.`navigators` WHERE (`email` = ?) and (`uuid` = ?)", use(email), use(uuid), now;
+        std::cout << "DELETING\n";
+        session << "DELETE FROM `rssreader`.`navigators` WHERE (`email` = '?') and (`uuid` = '?')", use(email), use(uuid), now;
+        std::cout << "DELETED\n";
         reqResp = new Poco::JSON::Object;
         reqResp->set("status", "OK");
     }catch(...){
