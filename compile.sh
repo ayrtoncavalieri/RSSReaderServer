51300da8d14c8b0751c8811e37db5449cb7729d6
Ayrton Cavalieri <ayrtoncavalieri@gmail.com>
2021-04-24 03:17:51 -0300

    Adding PocoXML library

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-g++ -g -O2 -DDEBUG -DSECSRV '-I src/' '-I src/opsImplementation' src/opsImplementation/*.cpp src/*.cpp -o RSSReaderServer.out -lPocoFoundation -lPocoNet -lPocoUtil -lPocoNetSSL -lPocoCrypto -lPocoData -lPocoDataMySQL -lPocoJSON -lPocoJWT
+g++ -g -O2 -DDEBUG -DSECSRV '-I src/' '-I src/opsImplementation' src/opsImplementation/*.cpp src/*.cpp -o RSSReaderServer.out -lPocoFoundation -lPocoNet -lPocoUtil -lPocoNetSSL -lPocoCrypto -lPocoData -lPocoDataMySQL -lPocoJSON -lPocoXML -lPocoJWT
