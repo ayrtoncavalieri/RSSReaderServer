a70a0cd5c0013d4dc15154c0ced3606ece7b3275
Ayrton Cavalieri <ayrtoncavalieri@gmail.com>
2021-04-29 00:47:07 -0300

    Fixed MySQL wrong use

--- src/opsImplementation/redefinePassword.cpp
+++ src/opsImplementation/redefinePassword.cpp
@@ -69,7 +69,7 @@ Poco::JSON::Object::Ptr redefPassword::redefine(unsigned int op, Poco::JSON::Obj
                 authIDJSON = p.parse(othersInfo).extract<Poco::JSON::Object::Ptr>();
                 authID = commonOps::genAuthID(128);
                 authIDJSON->set("authID", authID);
-                session << "UPDATE `rssreader`.`users` SET `othersInfo` = ? WHERE (`email` = ?)", use(userEmail), now;
+                session << "UPDATE `rssreader`.`users` SET `othersInfo` = ? WHERE (`email` = ?)", use(othersInfo), use(userEmail), now;
                 
                 Poco::Net::MailMessage mailMessage;
                 mailMessage.addRecipient(Poco::Net::MailRecipient(Poco::Net::MailRecipient::PRIMARY_RECIPIENT, userEmail));
